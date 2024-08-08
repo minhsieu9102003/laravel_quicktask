cacf884a0c3b6dc613952b2716689e40cb6678a6
Minh <minhla123123@gmail.com>
2024-08-08 07:57:51 +0700

    complete task7 middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CheckAdmin;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => CheckAdmin::class, // Registering the CheckAdmin middleware
+        ]);
+
+        // Optionally, you can append or prepend middleware to the global stack
+        // $middleware->append(CheckAdmin::class);
+        // $middleware->prepend(CheckAdmin::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,18 @@ use App\Http\Controllers\TaskController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/users',[UserController::class, 'index'])->name('users.index');
-Route::get('/users/create',[UserController::class, 'create'])->name('users.create');
-Route::get('/users/edit',[UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/{user}',[UserController::class, 'show'])->name('users.show');
-Route::post('/user',[UserController::class, 'store'])->name('users.store'); 
-Route::put('/users/{user}',[UserController::class, 'update'])->name('users.update'); 
-Route::delete('/users/{user}',[UserController::class, 'delete'])->name('users.delete'); 
 
-Route::resource('tasks',TaskController::class);
+// Apply the CheckAdmin middleware to a specific route or group
+Route::middleware(['admin'])->group(function () {
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    // Add other admin-only routes here
+});
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+Route::resource('tasks', TaskController::class);
