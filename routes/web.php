d93df93c5fe434750d254b2d605bf36654e20cbb
Minh <minhla123123@gmail.com>
2024-08-08 12:03:10 +0700

    complete task 9 localization

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,41 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/dashboard',function(){
+    return view('welcome');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')->group(function (){
+    Route::get('/profile',[ProfileController::class,'edit'])->name('profile.edit');
+    Route::patch('/profile',[ProfileController::class,'update'])->name('profile.update');
+    Route::delete('/profile',[ProfileController::class,'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth','admin'])->group(function (){
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/users/delete', [UserController::class, 'delete'])->name('users.delete');
+    Route::patch('/users/update', [UserController::class, 'update'])->name('users.update');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+   
+});
+
+
+
+Route::resource('tasks', TaskController::class);
+
+Route::get('language/{lang}', [LanguageController::class, 'changeLanguage'])->name('locale');
+
+require __DIR__ . '/auth.php';
